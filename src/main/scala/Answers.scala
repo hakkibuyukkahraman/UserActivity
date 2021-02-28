e621a56b07921e735e0ca68c8d5f290261670c8b
hakkibuyukkahraman <hakkibuyukkahraman@gmail.com>
2021-02-28 16:59:16 +0300

    History moved to README file

--- src/main/scala/Answers.scala
+++ src/main/scala/Answers.scala
@@ -1,22 +1,3 @@
-/**
- * Hakki Buyukkahraman
- * 06.02.2021
- * v1.0
- *
- * "Answers" object reads a csv file with columns "date, productId, eventName, userId"
- * and find answers to following questions.
- * 1- Unique Product View counts by ProductId
- * 2- Unique Event counts
- * 3- Top 5 Users who fulfilled all the events (view, add, remove, click)
- * 4- All events of #UserId: 47
- * 5- Product Views of #UserId: 47
- *
- * Notes:
- * 1. "connect" object creates 8 files by default. When defining the number of files as 1, no files are created.
- *    Hence, number of files is set to 2 for now. Issue is still under investigation.
- *
- */
-
 import org.apache.flink.api.scala.ExecutionEnvironment
 import org.apache.flink.table.api.DataTypes._
 import org.apache.flink.table.api.bridge.scala.BatchTableEnvironment
--- src/main/scala/AnswersDS.scala
+++ src/main/scala/AnswersDS.scala
@@ -1,23 +1,3 @@
-/**
- * Hakki Buyukkahraman
- * v1.0 - 06.02.2021 - Initial version
- * v1.1 - 28.02.2021 - DataSet API used instead of Table API
- *
- * "Answers" object reads a csv file with columns "date, productId, eventName, userId"
- * and find answers to following questions.
- * 1- Unique Product View counts by ProductId
- * 2- Unique Event counts
- * 3- Top 5 Users who fulfilled all the events (view, add, remove, click)
- * 4- All events of #UserId: 47
- * 5- Product Views of #UserId: 47
- *
- * Notes:
- * 1. "connect" object creates 8 files by default. When defining the number of files as 1, no files are created.
- *    Hence, number of files is set to 2 for now. Issue is still under investigation.
- * 2. "writeAsCsv" and "writeAsText" creates 8 files by default. When defining the parallelism as 1, "Access denied" error occurs.
- *    Hence, parallelism is set to 2 for now. Issue is still under investigation.
- */
-
 import org.apache.flink.api.common.operators.Order
 import org.apache.flink.api.scala.{ExecutionEnvironment, createTypeInformation}
 import org.apache.flink.core.fs.FileSystem
